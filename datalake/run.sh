52f9bc73538f2255321835f6fb65a6cb939bbc92
franc-ge <franco.nyawanga.consult@girleffect.org>
2024-03-03 22:28:50 +0300

    updated youtube

--- datalake/run.sh
+++ datalake/run.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
+
 error_handler() {
-  echo ${1} 1>&2
-  while caller $((n++)) 1>&2; do :; done
+  echo "${1}" 1>&2
+  echo "Failing command at ${BASH_SOURCE[1]}:${BASH_LINENO[0]}" 1>&2
+  echo 'Stack trace:' 1>&2
+  local n=1
+  while caller $n; do
+    ((n++))
+  done 1>&2
   exit 1
 }
-# trap error_handler ERR
 
+# Activate error handling
+# trap 'error_handler "An error occurred."' ERR
 
 if [[ "${SOURCE}" == "youtube" ]]; then
-    if [[ -z "${SECRETS_SAS_TOKEN}" ]] then
-        # error_handler 
+    if [[ -z "${SECRETS_SAS_TOKEN}" ]]; then
         echo 'Please provide SECRETS_SAS_TOKEN for secrets storage' 1>&2
         exit 1
     fi
 
-    if [[ -z "${SECRETS_FILE}" ]] then
-        # error_handler 
+    if [[ -z "${SECRETS_FILE}" ]]; then
         echo 'Please provide path to SECRETS_FILE' 1>&2
         exit 1
     fi
+    
+    mkdir -p ./GE_YT/secrets
 
     secrets_download=$(az storage blob download --no-progress --container-name you-tube \
-        --name $SECRETS_FILE --file GE_YT/secrets/youtube.json --account-name \
-        dpconfig --sas-token $SECRETS_SAS_TOKEN | jq)
+        --name "${SECRETS_FILE}" --file GE_YT/secrets/youtube.json --account-name \
+        dpconfig --sas-token "${SECRETS_SAS_TOKEN}" | jq -r '.')
 
-    printf "done downloading secrets file to GE_YT/secrets/youtube.json"
+    printf "done downloading secrets file to GE_YT/secrets/youtube.json\n"
 
     ls -l GE_YT/secrets
     echo ''

6f1f72a5099f8f155d7f816aa72b826fca3ed105
franc-ge <franco.nyawanga.consult@girleffect.org>
2024-03-04 00:27:33 +0300

    code clean up

--- datalake/run.sh
+++ datalake/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script is used to run data pipeline scripts based on the value of the SOURCE variable.
+# It handles error handling, downloads secrets files, and executes the data pipeline script accordingly.
+
 error_handler() {
   echo "${1}" 1>&2
   echo "Failing command at ${BASH_SOURCE[1]}:${BASH_LINENO[0]}" 1>&2
@@ -15,6 +18,7 @@ error_handler() {
 # trap 'error_handler "An error occurred."' ERR
 
 if [[ "${SOURCE}" == "youtube" ]]; then
+    WORKSPACE="GE_YT"
     if [[ -z "${SECRETS_SAS_TOKEN}" ]]; then
         echo 'Please provide SECRETS_SAS_TOKEN for secrets storage' 1>&2
         exit 1
@@ -24,20 +28,20 @@ if [[ "${SOURCE}" == "youtube" ]]; then
         echo 'Please provide path to SECRETS_FILE' 1>&2
         exit 1
     fi
-    
-    mkdir -p ./GE_YT/secrets
 
+    # Download secrets file from Azure Blob Storage
     secrets_download=$(az storage blob download --no-progress --container-name you-tube \
-        --name "${SECRETS_FILE}" --file GE_YT/secrets/youtube.json --account-name \
+        --name "${SECRETS_FILE}" --file "${WORKSPACE}"/secrets/youtube.json --account-name \
         dpconfig --sas-token "${SECRETS_SAS_TOKEN}" | jq -r '.')
 
-    printf "done downloading secrets file to GE_YT/secrets/youtube.json\n"
+    printf "done downloading secrets file to ${WORKSPACE}/secrets/youtube.json\n"
 
-    ls -l GE_YT/secrets
+    ls -l "${WORKSPACE}"/secrets
     echo ''
-    SECRETS_FILE="GE_YT/secrets/youtube.json"
+    SECRETS_FILE="${WORKSPACE}/secrets/youtube.json"
 
-    python -u "GE_YT/${PYSCRIPT:-datapipeline.py}" \
+    # Execute the data pipeline script
+    python -u "${WORKSPACE}/${PYSCRIPT:-datapipeline.py}" \
         --secrets_file="${SECRETS_FILE}" \
         --config_file="${CONFIG_FILE}" \
         --storage_account="${STORAGE_ACCOUNT}" \
@@ -45,4 +49,43 @@ if [[ "${SOURCE}" == "youtube" ]]; then
         --folder_path="${FOLDER_PATH}"
 
     echo "${SOURCE} script completed"
+    # echo "this is a test"
+fi
+
+if [[ "${SOURCE}" == "meta_engagement" ]]; then
+    WORKSPACE="GE_meta_engagement"
+    if [[ -z "${SECRETS_SAS_TOKEN}" ]]; then
+        echo 'Please provide SECRETS_SAS_TOKEN for secrets storage' 1>&2
+        exit 1
+    fi
+
+    if [[ -z "${SECRETS_FILE}" ]]; then
+        echo 'Please provide path to SECRETS_FILE' 1>&2
+        exit 1
+    fi
+
+    mkdir -p "${WORKSPACE}"/secrets
+
+    # Download secrets file from Azure Blob Storage
+    secrets_download=$(az storage blob download --no-progress --container-name meta \
+        --name "${SECRETS_FILE}" --file "${WORKSPACE}"/secrets/meta_engagement_secrets.json --account-name \
+        dpconfig --sas-token "${SECRETS_SAS_TOKEN}" | jq -r '.')
+
+    printf "done downloading secrets file to ${WORKSPACE}/secrets/meta_engagement_secrets.json\n"
+
+    ls -l "${WORKSPACE}"/secrets
+    echo ''
+    SECRETS_FILE="${WORKSPACE}/secrets/meta_engagement_secrets.json"
+
+    # Execute the data pipeline script
+    python -u "${WORKSPACE}/${PYSCRIPT:-datapipeline.py}" \
+        --secrets_file="${SECRETS_FILE}" \
+        --config_file="${CONFIG_FILE}" \
+        --storage_account="${STORAGE_ACCOUNT}" \
+        --container="${CONTAINER_NAME}" \
+        --folder_path="${FOLDER_PATH}" \
+        --folder_name="${FOLDER_NAME}"
+
+    echo "${SOURCE} script completed"
+    # echo "this is a test"
 fi

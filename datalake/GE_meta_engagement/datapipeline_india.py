8f06e19e0700b0212614a3d9260142fa98ff525c
bmusanga <61532768+bmusanga@users.noreply.github.com>
2024-11-14 09:34:38 +0300

    Update datapipeline_india.py

--- datalake/GE_meta_engagement/datapipeline_india.py
+++ datalake/GE_meta_engagement/datapipeline_india.py
@@ -1,103 +1,66 @@
-#!/usr/bin/python
-"""FACEBOOK API READER"""
-# import os
-import sys
-import copy
-import warnings
-import logging
-from typing import Generator, Any, Dict, List
-from datetime import datetime
+"""DATA PIPELINE CALL FOR META FACEBOOK"""
 
-from facebook_business.api import FacebookAdsApi
-from facebook_business.adobjects.page import Page
-from facebook_business.adobjects.user import User
-from facebook_business.adobjects.pagepost import PagePost
+import sys
+import time
+import argparse
+
+# from typing import Generator
 
 sys.path.append("../")
 
-from utils.date_handlers import string_to_date
-from utils.quota_handler import retry_handler, api_handler
-
-warnings.filterwarnings('ignore', category=UserWarning) 
-logger = logging.getLogger(__name__)
-
-class FacebookException(Exception):
-    """Base class exception for Facebook"""
+from GE_meta_engagement.reader_india import MetaReader, FacebookAPIAuthenticator
+from GE_meta_engagement.writer import MetaWriter
+from utils.file_handlers import load_file
 
 
-class PostEngagements:
-    """Class to read data from Facebook"""
+def main():
+    """main pipeline method"""
 
-    def __init__(self, authenticator, configs: dict):
-        self.authenticator = authenticator
-        self.configs = configs
+    parser = argparse.ArgumentParser(description="Data pipeline for Meta Facebook")
+    parser.add_argument("--secrets_file", type=str, help="path to secrets file")
+    parser.add_argument("--config_file", type=str, help="path to configs folder")
+    parser.add_argument("--storage_account", type=str, default="meta_engagement")
+    parser.add_argument("--container", type=str)
+    parser.add_argument("--folder_path", type=str, default=None)
+    parser.add_argument("--folder_name", type=str, default=None)
 
-    def build_query(self, configs: dict) -> Dict[str, Any]:
-        """method to build a query"""
-        expected_params = ["since", "until", "metric"]
-        params: dict = {}
-        for param in expected_params:
-            if value := configs.get(param):
-                if param in ["since", "until"]:
-                    value = string_to_date(value).strftime("%Y-%m-%d")
-                params[param] = value
-        return params
+    args, _ = parser.parse_known_args()
+    secrets_file = args.secrets_file
+    config_file = args.config_file
+    storage_account = args.storage_account
+    container = args.container
+    folder_path = args.folder_path
+    folder_name = args.folder_name
 
-    @staticmethod
-    def _process_insights_data(post_insights) -> list:
-        """get insights data as a list"""
-        insights_data: List[Any] = []
-        if post_insights:
-            _ = [insights_data.append(item._json) for item in post_insights]
-        return insights_data
-
-    @retry_handler(
-        exceptions=ConnectionError, initial_wait=3, total_tries=3, backoff_factor=2
+    configs = load_file(config_file)
+    authenticator = FacebookAPIAuthenticator(creds_file=secrets_file)
+    reader: MetaReader = MetaReader(authenticator=authenticator, configs=configs)
+    local_writer: MetaWriter = MetaWriter(     # noqa F841
+        container=container,
+        destination="local_json",
+        configs=configs,
+        clear_destination=True,
     )
-    @api_handler(wait=1, backoff_factor=2)
-    def post_insights(self, post: dict, insights_params: dict):
-        """make api call"""
-        try:
-            return PagePost(post["id"]).get_insights(fields=[], params=insights_params)
-        except ConnectionError as err:
-            raise ConnectionError(f"Connection Error: {err}") from err
+    azure_writer: MetaWriter = MetaWriter(
+        container=container,
+        destination="azure_json",
+        configs={
+            "storage_account": storage_account,
+            "overwrite": True,
+            "auth_method": "sas_token",
+        },
+        clear_destination=False,
+    )
 
-    def get_insights(
-        self, page: Page, configs: dict
-    ) -> Generator[Dict[str, Any], None, None]:
-        """Fetch insights for all posts of a page"""
+    for res in reader.query():
+        # local_writer.sink(payload=res, folder_path=folder_path, folder_name=folder_name, indent=None)
+        azure_writer.sink(
+            payload=res, folder_path=folder_path, folder_name=folder_name, indent=None
+        )
 
-        fields: List[str] = configs["fields"]
-        params: Dict[str, Any] = {}
-        FacebookAdsApi.init(access_token=page["access_token"], debug=False)
 
-        for post in Page(page.get_id()).get_posts(fields=fields, params=params):
-            json_data = copy.copy(post)._json
-            insights_params: Dict[str, Any] = self.build_query(configs)
-
-            post_insights = self.post_insights(post, insights_params)
-            data = self._process_insights_data(post_insights)
-            json_data["insights"] = data
-            pull_date = datetime.today()
-            date_partition: str = pull_date.strftime("%Y/%m")
-            date_string: str = pull_date.strftime("%Y-%m-%d")
-
-            file_name: str = (
-                f"{page['id']}/{post['id']}/{date_partition}/{date_string}-test-{post['id']}"
-            )
-            yield {
-                "page_id": page["id"],
-                "page_name": page["name"],
-                "post_id": post["id"],
-                "data": json_data,
-                "date": datetime.now().strftime("%Y-%m-d%"),
-                "file_name": file_name,
-            }
-            logger.info(f"Post insights fetched: {post['id']}")
-
-    def get_data(self) -> Generator[Dict[str, Any], None, None]:
-        """Get insights for all pages"""
-        # Retrieve insights for each page associated with the user
-        user_id: int = self.authenticator.creds.get("user_id")
-        for page in User(user_id).get_accounts(fields=[], params={}):
-            yield from self.get_insights(page, configs=self.configs)
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    main()
+    end_time = time.perf_counter()
+    print(f"Elapsed run time: {end_time - start_time} seconds.")
